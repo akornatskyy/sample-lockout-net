4e40b7d42e0d5d8949c72be36d669c55034b46fe
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-09 19:12:13 +0200

    Added lockout service with status quota and guard lockouts.

---
+++ src/Service/Bridge/LockoutService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Models;
+using Repository.Interface;
+using Service.Interface;
+
+namespace Service.Bridge
+{
+    public sealed class LockoutService : ILockoutService
+    {
+        private static readonly LockoutRuntime StatusLockoutQuota = new LockoutRuntime(new[]
+        {
+            new LockoutDef { Key = "Status-Quota: {ip}-", Threshold = 100, Expiration = 15 * 60 },
+            new LockoutDef { Key = "Status-Quota: -{name}", Threshold = 20, Expiration = 5 * 60 },
+            new LockoutDef { Key = "Status-Quota: {ip}-{name}", Threshold = 10, Expiration = 60 }
+        });
+
+        private static readonly LockoutRuntime StatusLockoutGuard = new LockoutRuntime(new[]
+        {
+            new LockoutDef { Key = "Status-Guard: -{name}", Threshold = 10, Expiration = 5 * 60 },
+            new LockoutDef { Key = "Status-Guard: {ip}-{name}", Threshold = 4, Expiration = 60 }
+        });
+
+        private readonly ICounterRepository counterRepository;
+
+        public LockoutService(ICounterRepository counterRepository)
+        {
+            this.counterRepository = counterRepository;
+        }
+
+        public Task<bool> StatusQuota(IDictionary<string, string> context)
+        {
+            Debug.Assert(context.ContainsKey("name"), "name");
+            Debug.Assert(context.ContainsKey("ip"), "ip");
+            return LockoutService.StatusLockoutQuota.Guard(this.counterRepository, context);
+        }
+
+        public Task<bool> StatusCheck(IDictionary<string, string> context)
+        {
+            Debug.Assert(context.ContainsKey("name"), "name");
+            Debug.Assert(context.ContainsKey("ip"), "ip");
+            return LockoutService.StatusLockoutGuard.Check(this.counterRepository, context);
+        }
+
+        public Task<bool> StatusGuard(IDictionary<string, string> context)
+        {
+            Debug.Assert(context.ContainsKey("name"), "name");
+            Debug.Assert(context.ContainsKey("ip"), "ip");
+            return LockoutService.StatusLockoutGuard.Guard(this.counterRepository, context);
+        }
+    }
+}
---
+++ src/Service/Interface/ILockoutService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface ILockoutService
+    {
+        Task<bool> StatusQuota(IDictionary<string, string> context);
+
+        Task<bool> StatusCheck(IDictionary<string, string> context);
+
+        Task<bool> StatusGuard(IDictionary<string, string> context);
+    }
+}
