0f99535020119507792f92928ceaca6ee8ff0598
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-10 22:02:50 +0200

    Added HTTP request message extension for client ip address.

---
+++ src/Web/Infrastructure/HttpRequestMessageExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace Web.Infrastructure
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static string ClientIpAddress(this HttpRequestMessage req)
+        {
+            var ip = TryGetLastHttpHeaderValue(req, "X-Forwarded-For");
+            if (!string.IsNullOrEmpty(ip))
+            {
+                return ip;
+            }
+
+            if (req.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)req.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            
+            return HttpContext.Current != null ? HttpContext.Current.Request.UserHostAddress : null;
+        }
+
+        private static string TryGetLastHttpHeaderValue(HttpRequestMessage request, string headerName)
+        {
+            IEnumerable<string> values;
+            if (!request.Headers.TryGetValues(headerName, out values))
+            {
+                return null;
+            }
+
+            var value = values.LastOrDefault();
+            if (string.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+
+            return value.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
+        }
+    }
+}
