55169dfe8a95e160ed947a2bdeea782140078711
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-11 21:33:45 +0200

    Updated unity config with repository and service registration.

--- src/Web/App_Start/UnityConfig.cs
+++ src/Web/App_Start/UnityConfig.cs
@@ -3,6 +3,11 @@ using System.Web.Http;
 using Microsoft.Practices.Unity;
 using Unity.WebApi;
 
+using Repository.Interface;
+using Repository.Mock;
+using Service.Bridge;
+using Service.Interface;
+
 namespace Web
 {
     public static class UnityConfig
@@ -15,12 +20,14 @@ namespace Web
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
 
-        private static void RegisterRepositories(IUnityContainer container)
+        private static void RegisterRepositories(IUnityContainer c)
         {
+            c.RegisterType<ICounterRepository, CounterRepository>(new ContainerControlledLifetimeManager());
         }
 
-        private static void RegisterServices(IUnityContainer container)
+        private static void RegisterServices(IUnityContainer c)
         {
+            c.RegisterType<ILockoutService, LockoutService>(new HierarchicalLifetimeManager());
         }
     }
 }
