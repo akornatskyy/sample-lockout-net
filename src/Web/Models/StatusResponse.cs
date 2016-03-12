ed35925df2587b040b90dfbd809a7a791384200e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-12 16:19:20 +0200

    Updated status response with client ip address; added locked status response.

--- src/Web/Models/StatusResponse.cs
+++ src/Web/Models/StatusResponse.cs
@@ -2,6 +2,10 @@
 {
     public sealed class StatusResponse
     {
+        public static readonly StatusResponse Locked = new StatusResponse();
+
         public bool IsAuthenticated { get; set; }
+
+        public string IpAddress { get; set; }
     }
 }
