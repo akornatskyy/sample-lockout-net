987f99652a80171d1a7d71fe2ac9dca605bd9152
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-12 15:17:20 +0200

    Updated status request with lockout context.

--- src/Web/Models/StatusRequest.cs
+++ src/Web/Models/StatusRequest.cs
@@ -1,9 +1,12 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Web.Models
 {
     public sealed class StatusRequest
     {
         public ClaimsPrincipal User { get; set; }
+
+        public IDictionary<string, string> LockoutContext { get; set; }
     }
 }
