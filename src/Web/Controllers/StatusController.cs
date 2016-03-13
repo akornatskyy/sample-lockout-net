a764a39e8ccc1e102df06d719fe473ec71ac6c7d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-13 11:00:46 +0200

    Updated status controller and facade with support for lockout.

--- src/Web/Controllers/StatusController.cs
+++ src/Web/Controllers/StatusController.cs
@@ -1,8 +1,12 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 using Web.Constants;
+using Web.Infrastructure;
 using Web.Facade;
 using Web.Models;
 
@@ -11,12 +15,28 @@ namespace Web.Controllers
     [Route(RoutePatterns.Status)]
     public sealed class StatusController : Infrastructure.ApiController
     {
-        public async Task<StatusResponse> Get()
+        public async Task<IHttpActionResult> Get()
         {
-            return await this.GetService<StatusFacade>().RefreshStatus(new StatusRequest
+            var response = await this.GetService<StatusFacade>().RefreshStatus(new StatusRequest
             {
-                User = (ClaimsPrincipal)User
+                User = (ClaimsPrincipal)User,
+                LockoutContext = new Dictionary<string, string>
+                {
+                    { "ip", this.Request.ClientIpAddress() }
+                }
             });
+
+            if (response == StatusResponse.Locked)
+            {
+                return this.ResponseMessage(this.Request.CreateErrorResponse(
+                    HttpStatusCode.Forbidden, "The service is temporarily unavailable."));
+            }
+            else if (response == null)
+            {
+                return this.BadRequest();
+            }
+
+            return this.Ok(response);
         }
     }
 }
--- src/Web/Facade/StatusFacade.cs
+++ src/Web/Facade/StatusFacade.cs
@@ -1,17 +1,48 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
+using Service.Interface;
 using Web.Models;
 
 namespace Web.Facade
 {
-    public class StatusFacade
+    public sealed class StatusFacade
     {
-        public Task<StatusResponse> RefreshStatus(StatusRequest request)
+        private static readonly Random Rnd = new Random();
+
+        private readonly ILockoutService lockoutService;
+
+        public StatusFacade(ILockoutService lockoutService)
         {
-            return Task.FromResult(new StatusResponse
+            this.lockoutService = lockoutService;
+        }
+
+        public async Task<StatusResponse> RefreshStatus(StatusRequest request)
+        {
+            var identity = request.User.Identity;
+            request.LockoutContext["name"] = identity.Name;
+            if (await this.lockoutService.StatusQuota(request.LockoutContext))
             {
-                IsAuthenticated = request.User.Identity.IsAuthenticated
-            });
+                return StatusResponse.Locked;
+            }
+
+            if (await this.lockoutService.StatusCheck(request.LockoutContext))
+            {
+                return StatusResponse.Locked;
+            }
+
+            // Simulate an error
+            if (Rnd.Next(10) > 5)
+            {
+                await this.lockoutService.StatusGuard(request.LockoutContext);
+                return null;
+            }
+
+            return new StatusResponse
+            {
+                IsAuthenticated = identity.IsAuthenticated,
+                IpAddress = request.LockoutContext["ip"]
+            };
         }
     }
 }
